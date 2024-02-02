b4804eae703ac34acc3c5b61e8e20bc692da8033
AnastasiiaRudch <anastasiag1409@gmail.com>
2024-02-02 19:56:48 +0200

    adding docker, eureka modules; adding base settings

---
+++ src/test/java/com/example/gameconfig/GameConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gameconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
